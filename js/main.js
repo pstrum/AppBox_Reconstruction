581f5ca8bcc305dbff704c78c190877a079185c5
James Norton <james.norton@me.com>
2015-09-25 09:08:46 -0700

    Change slider to display other apps section more accurately

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(function() {
       slidesPerView: "auto",
       pagination: ".swiper-pagination-2",
       paginationClickable: true,
-      centeredSlides: true,
+      centeredSlides: false,
       loop: true
     });
   }
