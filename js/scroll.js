b8dec61d6101317e714ba9b595b0a321ab48c1a6
Chris Holt <christopher.d.holt@gmail.com>
2015-09-25 11:44:35 -0700

    fixed menu scrolling to top when closed by adjusting href of close nav to menuToggle (added to menu toggle link)

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,11 @@
 $(document).ready(function ()  {
-  $('#nav .nav-link').smoothScroll({
-    offset: -150,
-    speed: 1000,
-    exclude: ['.close-nav','#top']
-  });
+    $('#nav .nav-link').smoothScroll({
+      offset: -150,
+      speed: 1000,
+    });
 
-  $('.to-top').smoothScroll({
-    direction: 'top',
-    speed: 1000
-  });
+    $('.to-top').smoothScroll({
+      direction: 'top',
+      speed: 1000
+    });
 });
